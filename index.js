74a700c06ebcbd3bf6a4297431f675220431ac4f
MightyEST <hendriksonkristen1@gmail.com>
2020-11-30 15:06:22 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 // Import stylesheets
-import './style.css';
+import "./style.css";
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+const appDiv = document.getElementById("app"); // = Select element with id "app"
+appDiv.innerHTML = `<h1>JS Starter123</h1>`; // Inject something into that element
+
+function reqListener() {
+  console.log(this.responseText);
+}
+
+var oReq = new XMLHttpRequest();
+oReq.addEventListener("load", reqListener);
+oReq.open(
+  "GET",
+  "http://en.wikipedia.org/api/rest_v1/page/summary/Viljandi_County"
+);
+oReq.send();
